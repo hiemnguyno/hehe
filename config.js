a3988e222ba79b031a33a21078c1b93bf3ea4682
hiemnguyno <69081691+hiemnguyno@users.noreply.github.com>
2021-09-29 21:15:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'introTitle',
-    introDesc: `introDesc`,
-    btnIntro: 'btnIntro',
-    title: 'title',
-    desc: 'desc',
-    btnYes: 'btnYes',
-    btnNo: 'btnNo',
-    question:'question',
-    btnReply: 'btnReply',
-    reply: 'reply',
-    mess: 'mess',
-    messDesc: 'messDesc',
-    btnAccept: 'btnAccept',
-    messLink: 'messLink', //link fb
+    introTitle: 'Đoán xem có gì nè bé :))',
+    introDesc: `ngồi làm sáng giờ :))`,
+    btnIntro: 'vào tự mè xem đi :))',
+    title: 'Bé có yêu anh nhìu hong :>',
+    desc: 'thử ấn ko xem',
+    btnYes: 'dạ có!',
+    btnNo: 'Ừ,không',
+    question:'lí do bé yêu anh là gì nò',
+    btnReply: 'send thoi :))',
+    reply: 'Vì anh đập try vl :)',
+    mess: 'Anh cũng iu bé nhìu :<',
+    messDesc: 'biết ngay mình đập try quá mà :))',
+    btnAccept: 'ib với anh thì ấn here :>',
+    messLink: 'https://www.facebook.com/messages/t/100069153999472', //link fb
     bgLink: 'https://huypham9205.github.io/tang-crush/img/bg.webp', //link ảnh nền
     iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
     lookMeLink: 'https://huypham9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
