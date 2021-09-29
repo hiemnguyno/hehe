768f4a8cc816dafa813796acd4253747b68329be
hiemnguyno <69081691+hiemnguyno@users.noreply.github.com>
2021-09-29 21:25:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ const CONFIG = {
     messDesc: 'biết ngay mình đập try quá mà :))',
     btnAccept: 'ib với anh thì ấn here :>',
     messLink: 'https://www.facebook.com/messages/t/100069153999472', //link fb
-    bgLink: 'https://huypham9205.github.io/tang-crush/img/bg.webp', //link ảnh nền
-    iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
-    lookMeLink: 'https://huypham9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
+    bgLink: 'https://cdn.myanimelist.net/s/common/uploaded_files/1460603253-2b93f5f88137a80252be295d6fb7be43.jpeg', //link ảnh nền
+    iputBgLink: 'https://i.pinimg.com/originals/20/5b/71/205b712184084a649e5c39035d05a873.jpg', //link iputBgLink
+    lookMeLink: 'https://ih1.redbubble.net/image.1114362285.4038/flat,750x,075,f-pad,750x1000,f8f8f8.jpg', //link ảnh lookMe
     soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
     duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
     swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
