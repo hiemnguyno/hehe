7b900a84bbe53b9e4d342808de757e77d81f3f44
hiemnguyno <69081691+hiemnguyno@users.noreply.github.com>
2021-10-21 13:36:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     messDesc: 'biết ngay mình đập try quá mà :))',
     btnAccept: 'ib với anh thì ấn here :>',
     messLink: 'https://www.facebook.com/messages/t/100069153999472', //link fb
-    bgLink: 'https://github.com/hiemnguyno/hehe/background.jpeg', //link ảnh nền
+    bgLink: 'https://toigingiuvedep.vn/wp-content/uploads/2021/05/hinh-anh-i-love-you.jpg', //link ảnh nền
     iputBgLink: 'https://i.pinimg.com/originals/20/5b/71/205b712184084a649e5c39035d05a873.jpg', //link iputBgLink
     lookMeLink: 'https://media.discordapp.net/attachments/853961664679116803/900632508645904384/USER_SCOPED_TEMP_DATA_orca-image-756855965.jpeg?width=504&height=640', //link ảnh lookMe
     soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
